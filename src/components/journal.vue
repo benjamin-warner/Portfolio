e1278932ffc94ba339e0fb6ce6e36989ef846b48
Ben Warner <benjaminnwarner@gmail.com>
2018-02-11 20:58:17 -0500

    Journal page added

---
+++ src/components/journal.vue
@@ -0,0 +1,26 @@
+<template>
+
+	<div id="journal">
+		<h1> {{ title }}</h1>
+	</div>
+
+</template>
+
+<script>
+
+export default{
+	name: 'Journal',
+	data(){
+		return {
+			title: 'Hello journal'
+		}
+	}
+}
+
+</script>
+
+
+<style>
+
+
+</style>
