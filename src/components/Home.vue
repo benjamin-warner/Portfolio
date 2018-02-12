a9f5328ca23a3647421d0e6c9e5b548857392eaf
Ben Warner <benjaminnwarner@gmail.com>
2018-02-11 20:26:02 -0500

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+
+	<div id="home">
+		<h1> {{ title }}</h1>
+	</div>
+
+</template>
+
+<script>
+
+export default{
+	name: 'Home',
+	data(){
+		return {
+			title: 'Hello home'
+		}
+	}
+}
+
+</script>
+
+
+<style>
+
+
+</style>
