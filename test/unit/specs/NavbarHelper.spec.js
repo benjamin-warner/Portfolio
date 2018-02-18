779b4fdf3c12148d819c823bff9e754eb256b643
Ben Warner <benjaminnwarner@gmail.com>
2018-02-18 14:10:01 -0500

    Update 2018-02-18T19:09:57.146Z

--- test/unit/specs/NavbarHelper.spec.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Router from '../../src/router'
-import * from '../../src/helpers/NavbarHelper'
-
-describe('NavbarHelper.js', () => {
-  it('should load router elements into navbar', () => {
-  		???
-  })
-})
